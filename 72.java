efad6abb438be9e1670a056d8a099ba879eda44b
Harshitha-0531 <meruguharshitha6@gmail.com>
2025-09-10 10:53:45 +0530

    edit distance

---
+++ 72.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minDistance(String word1, String word2) { 
+        int m=word1.length();
+        int n=word2.length();
+        int arr[][]=new int[m+11][n+1];
+        for (int j=0;j<=n;j++){
+            arr[0][j]=j;
+        }
+        for (int i=0;i<=m;i++){
+            arr[i][0]=i;
+        }
+        for (int i=1;i<=m;i++){
+            for (int j=1;j<=n;j++){
+                if (word1.charAt(i-1)==word2.charAt(j-1)){
+                    arr[i][j]=arr[i-1][j-1];
+                }else{
+                    arr[i][j]= 1+ Math.min( arr[i-1][j],Math.min( arr[i][j-1], arr[i-1][j-1]));
+                }
+            }
+        } 
+      
+        return arr[m][n];  
+    }
+}
