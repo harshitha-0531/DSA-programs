ae0d50d7626691cddae1417e2007d40a8f06b84b
Harshitha-0531 <meruguharshitha6@gmail.com>
2025-09-15 11:21:19 +0530

    delete operations

---
+++ 583.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int row=word1.length();
+        int col=word2.length();
+        int dp[][]=new int[row+1][col+1];
+
+        for (int i=0;i<=row;i++){
+            dp[i][0]=i;
+        }
+        for (int j=1;j<=col;j++){
+            dp[0][j]=j;
+
+        }
+        for (int i=1;i<=row;i++){
+            for (int j=1;j<=col;j++){
+                if (word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+
+                }else{
+                     int top=dp[i-1][j];
+                     int left=dp[i][j-1];
+                    dp[i][j]=Math.min(top,left)+1;
+                    // dp[i][j]=Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
+                }
+            }
+        }
+        return dp[row][col];  
+    }
+}
