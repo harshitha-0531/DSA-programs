3f6fc9f9dc87d10f0bd52d2aaffd6d3e4351d4ba
Harshitha-0531 <meruguharshitha6@gmail.com>
2026-01-05 10:11:59 +0530

    Implement path sum algorithm using BFS

---
+++ 112.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null){ return false ; }
+        
+        Queue <TreeNode> path=new LinkedList<>();
+        Queue<Integer> sum=new LinkedList<>();
+        path.offer(root);
+        sum.add(root.val);
+        while(!path.isEmpty()){
+            TreeNode node=path.poll();
+            int nodesum=sum.poll();
+            if (node.left!=null){
+                path.add(node.left);
+                sum.add(node.left.val+nodesum);
+            }if (node.right!=null){
+                path.add(node.right);
+                sum.add(node.right.val+nodesum);
+            }
+            
+            if (nodesum==targetSum && node.left==null && node.right==null){
+                return  true;
+              
+            }
+        }
+        return false;
+    }
+}
