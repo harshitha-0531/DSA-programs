1e9c4fb46925d8ab85bc6a4109cdaaf96bf42459
Harshitha-0531 <meruguharshitha6@gmail.com>
2026-01-07 12:13:27 +0530

    countNodes to use depth for counting
    Refactor countNodes method to use depth calculation for counting nodes in a complete binary tree.

--- 222.java
+++ 222.java
@@ -13,22 +13,53 @@
  *     }
  * }
  */
-class Solution {
-    public int countNodes(TreeNode root) {
-        if (root==null){return 0;}
-        Queue<TreeNode> path=new LinkedList<>();
-        int count=0;
-        path.offer(root);
-        while(!path.isEmpty()){
-            TreeNode node=path.poll();
-            count++;
-            if (node.left!=null){
-                path.add(node.left);
-            }
-            if (node.right!=null){
-                path.add(node.right);
-            }
+// class Solution {
+//     public int countNodes(TreeNode root) {
+//         if (root==null){return 0;}
+//         Queue<TreeNode> path=new LinkedList<>();
+//         int count=0;
+//         path.offer(root);
+//         while(!path.isEmpty()){
+//             TreeNode node=path.poll();
+//             count++;
+//             if (node.left!=null){
+//                 path.add(node.left);
+//             }
+//             if (node.right!=null){
+//                 path.add(node.right);
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+
+class Solution{
+    public int countNodes(TreeNode root){
+        int leftdep=leftdepth(root);
+        int rightdep=rightdepth(root);
+        if (leftdep == rightdep){
+            return (int) Math.pow(2,leftdep)-1;
+        }else{
+            return 1+countNodes(root.left)+countNodes(root.right);
         }
-        return count;
+    
     }
+    public int leftdepth(TreeNode root){
+        int dep=0;
+        while(root!=null){
+            dep++; 
+            root=root.left;
+        }
+        return dep;
+    }
+    public int rightdepth(TreeNode root){
+        int dep=0;
+        while(root!=null){
+            dep++; 
+            root=root.right;
+        }
+        return dep;
+    }
+    
 }
